c318b7732f1d9c24f1aca73f661cf8aa5e508f36
Elaine <71743181+elainechan01@users.noreply.github.com>
2021-11-20 18:54:03 -0500

    init

---
+++ foodpath/Shared/foodpathApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodpathApp.swift
+//  Shared
+//
+//  Created by Eugene Tye on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct foodpathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
