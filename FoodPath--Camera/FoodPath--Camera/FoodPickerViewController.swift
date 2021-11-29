4050f53794931642dd10957db0d14e57c6cb6365
Kim C <43968366+KimChheu1@users.noreply.github.com>
2021-11-28 21:41:40 -0500

    Search API can recommend dishes based on checked recipes

--- FoodPath--Camera/FoodPath--Camera/FoodPickerViewController.swift
+++ FoodPath--Camera/FoodPath--Camera/FoodPickerViewController.swift
@@ -6,24 +6,144 @@
 //
 
 import UIKit
+import Parse
 
-class FoodPickerViewController: UIViewController {
+class FoodPickerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var possibleFoods = [PFObject]()
+    var foodSuggestions = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        callSearchAPI()
+       }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "getFoodFromAPI")
+        query.findObjectsInBackground(block: { (foods, error) in
+            if foods != nil{
+                self.possibleFoods = foods!
+                self.tableView.reloadData()
+            }
+            
+        })
     }
     
+    
+func callSearchAPI(){
+    let checkedIngredients = PFQuery(className: "checkIngredients")
 
-    /*
-    // MARK: - Navigation
+    var userChecked: [String] = []
+    var searchTerm: String = " "
+    
+    // Retrieve checked ingredients from database
+    checkedIngredients.findObjectsInBackground{ (objects, error) -> Void in
+        if error == nil{
+            if let returnedObjects = objects{
+                for object in returnedObjects{
+                    if (object["isChecked"] != nil){
+                        userChecked.append(object["ingredientName"] as! String)
+                    }
+                }
+            }
+            
+            for (index, checkedItem) in userChecked.enumerated(){
+                searchTerm.append(checkedItem)
+                if index < userChecked.count-1{
+                    searchTerm.append("+")
+                }
+            }
+            
+            
+            
+            //Set up URL Query to call search API
+            var urlComponents = URLComponents()
+            urlComponents.scheme = "https"
+            urlComponents.host = "serpapi.com"
+            urlComponents.path = "/search"
+            urlComponents.queryItems = [
+                URLQueryItem(name: "q", value: searchTerm),
+                URLQueryItem(name: "api_key", value: "4c43411d817856122333fbeba420b7141241fdd65894d3144add0c4e1467bcdd")
+            ]
+            guard let someString = urlComponents.url?.absoluteString else { return  }
+        
+        
+            let url = URL(string: someString)!
+            let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+            let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request) { (data, response, error) in
+                 if let error = error {
+                        print(error.localizedDescription)
+                 } else if let data = data {
+                        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        
+                     self.foodSuggestions = dataDictionary["recipes_results"] as! [[String:Any]]
+                     let returnedFoods = self.foodSuggestions
+                     
+                     
+                     
+        
+                      // Add each result to database
+                     for (index,foodItem) in returnedFoods.enumerated(){
+                         let foodFromAPI = PFObject(className: "getFoodFromAPI")
+                         let possibleRecipe = foodItem["title"] as! String
+                       
+                         foodFromAPI["foodName"] = possibleRecipe
+                         foodFromAPI["foodId"] = index + 1
+                         foodFromAPI.saveInBackground { (success, error) in
+                             if success{
+                                 print("Saved!")
+                             }
+                             else{
+                                 print("Error: \(String(describing: error?.localizedDescription))")
+                             }
+                         }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                     }
+                     
+                     
+                 }
+            }
+            task.resume()
+         
+        }
+        else{
+            print("Error: Empty object")
+        }
+        
     }
-    */
 
+
+}
+    
+   
+    
+    
+
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return possibleFoods.count
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeOptionCell") as! RecipeOptionCell
+    
+    let ingredient = possibleFoods[indexPath.row]
+    cell.recipeChoiceLabel.text = ingredient["foodName"] as! String
+    
+    return cell
+    
+    }
+
+    
+    
 }
